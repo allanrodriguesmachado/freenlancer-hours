61f793d80b4959a6afcb3b233f0ce5fd8eca4204
Consultoria 01 <consultoria01@shibata.corp.br>
2024-12-27 14:03:21 -0300

    Config Invokable Controller

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function __invoke(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    {
+        $user = User::query()->create([
+            'name' => 'John Doe',
+            'email' => 'john@doee.com',
+            'password' => bcrypt('password')
+        ]);
+
+        dd($user);
+
+        return view('welcome');
+    }
+}
