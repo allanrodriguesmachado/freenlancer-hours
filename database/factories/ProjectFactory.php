5bbc0b864480550c118b703e2858b88fd2ef8d8e
Allan Rodrigues <consultoria01@shibata.corp.br>
2024-12-27 16:11:22 -0300

    Creating Seed, Migrate and factory - DB: Users, Projects

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->words(5, true),
+            'description' => fake()->randomHtml(),
+            'created_at' => fake()->dateTime(),
+            'ends_at' => fake()->dateTime(),
+            'status' => fake()->randomElement(['open', 'closed']),
+            'tech_stack' => fake()->randomElement([
+                'PHP',
+                'Java',
+                'Kotlin',
+                'C#',
+                'Laminas',
+                'Laravel'
+            ], random_int(1, 5)),
+            'created_by' => User::factory(),
+        ];
+    }
+}
