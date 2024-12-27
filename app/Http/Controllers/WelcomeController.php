20c601d8679a6e460a6479a922c21069b0a8e500
Allan Rodrigues <consultoria01@shibata.corp.br>
2024-12-27 14:17:50 -0300

    Cleanup Project

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use Illuminate\Contracts\View\{Factory, View};
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function __invoke(Request $request): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    public function __invoke(Request $request): View|Factory|Application
     {
-        $user = User::query()->create([
-            'name' => 'John Doe',
-            'email' => 'john@doee.com',
-            'password' => bcrypt('password')
-        ]);
-
         return view('welcome');
     }
 }
